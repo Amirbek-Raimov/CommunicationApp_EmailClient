ca6b5666e73b92a99f2026fbb9688dad889b17d9
Amirbek-Raimov <53548370+Amirbek-Raimov@users.noreply.github.com>
2021-06-08 00:19:59 -0700

    Configuring stage actions

--- src/com/amirbek/view/ViewFactory.java
+++ src/com/amirbek/view/ViewFactory.java
@@ -1,6 +1,15 @@
 package com.amirbek.view;
 
 import com.amirbek.EmailManager;
+import com.amirbek.controller.BaseController;
+import com.amirbek.controller.LoginWindowController;
+import com.amirbek.controller.MainWindowController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ViewFactory {
     private EmailManager emailManager;
@@ -10,6 +19,36 @@ public class ViewFactory {
     }
     public void showLoginWindow(){
         System.out.println("show login window called");
+        BaseController controller = new LoginWindowController(emailManager, this,"LoginWindow.fxml");
+        initializeStage(controller);
     }
+
+    public void showMainWindow(){
+        System.out.println("main window called");
+        BaseController controller = new MainWindowController(emailManager, this,"MainWindow.fxml");
+        initializeStage(controller);
+    }
+
+    private void initializeStage(BaseController baseController){
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(baseController.getFxmlName()));
+        fxmlLoader.setController(baseController);
+        Parent parent;
+        try{
+            parent=fxmlLoader.load();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+            return;
+        }
+        Scene scene = new Scene(parent);
+        Stage stage =new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void closeStage(Stage stageToClose){
+        stageToClose.close();
+    }
+
 
 }
