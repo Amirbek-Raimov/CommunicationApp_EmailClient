82ae519c63033250db9a4b24d8e30a16d6cdcbad
Amirbek-Raimov <53548370+Amirbek-Raimov@users.noreply.github.com>
2021-06-16 10:28:02 -0700

    Update LoginWindowController.java

--- src/com/amirbek/controller/LoginWindowController.java
+++ src/com/amirbek/controller/LoginWindowController.java
@@ -74,7 +74,7 @@ public class LoginWindowController extends BaseController implements Initializab
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        emailAddressFied.setText("amirbek.projects@gmail.com");
-        passwordField.setText("Dracula888***");
+        emailAddressFied.setText("provide_email@gmail.com");
+        passwordField.setText("***********");
     }
 }
